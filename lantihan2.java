b3e5e7b1f512ea64c8aa78c32910fbd71fab2469
erik-candra125 <erikcandra123987@gmail.com>
2024-11-16 13:23:21 +0700

    Add files via upload

---
+++ lantihan2.java
@@ -0,0 +1,10 @@
+public class lantihan2 {
+    public static void main (String[] args){
+
+    String buah1="anggur";
+    String buah2 = "jamu";
+
+    System.out.println(buah1.isEmpty());
+    System.out.println(buah2.isEmpty());
+    }
+}
